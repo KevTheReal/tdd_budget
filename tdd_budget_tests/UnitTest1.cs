14b0f8a26d2a3fce1162c0969338f13f38f3c4f2
TITANSOFT\kevin.cheng18 <kevin.cheng@titansoft.com.sg>
2023-12-21 17:21:57 +0800

    add test

---
+++ tdd_budget_tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using FluentAssertions;
+using NSubstitute;
+using tdd_budget.Services;
+
+namespace tdd_budget_tests;
+
+public class BudgetServiceTests
+{
+    private IBudgetRepo _budgetRepo;
+
+    [Test]
+    public void QueryWholeMonth()
+    {
+        _budgetRepo = Substitute.For<IBudgetRepo>();
+        _budgetRepo.GetAll().Returns(new List<Budget>()
+        {
+            new Budget
+            {
+                YearMonth = "202312",
+                Amount = 3100
+            }
+        });
+        var budgetService = new BudgetService(_budgetRepo);
+        var result = budgetService.Query(new DateTime(2023, 12, 1), new DateTime(2023, 12, 31));
+        result.Should().Be(3100);
+    }
+}
